0a9811cfa60c1b666724573bf22426b359772fd3
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-10-02 15:27:38 -0600

    Add conditional for not creating a user if the email is taken or passwords not matching

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -9,23 +9,42 @@ const uuidAPIKey = require('uuid-apikey');
 router.post('/', function(req, res, next) {
   res.setHeader("Content-Type", "application/json")
 
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    users.create({
-      email: req.body.email,
-      password_digest: hash,
-      api_key: uuidAPIKey.create().apiKey
-    })
+  if (req.body.password != req.body.password_confirmation) {
+    payload = {
+      error: "Passwords do not match",
+      status: 400
+    }
+    res.status(400).send(JSON.stringify(payload))
+  } else {
+    users.findOne({where: {email: req.body.email}})
     .then(user => {
-      api_key = {
-        api_key: user.api_key
+      if (user) {
+        payload = {
+          error: "Email has been taken",
+          status: 400
+        }
+        res.status(400).send(JSON.stringify(payload))
+      } else {
+        bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+          users.create({
+            email: req.body.email,
+            password_digest: hash,
+            api_key: uuidAPIKey.create().apiKey
+          })
+          .then(user => {
+            api_key = {
+              api_key: user.api_key
+            }
+            res.status(201).send(api_key)
+          })
+          .catch(error => {
+            console.log(error)
+            res.status(500).send({ error })
+          })
+        })
       }
-      res.status(201).send(api_key)
     })
-    .catch(error => {
-      console.log(error)
-      res.status(500).send({ error })
-    })
-  })
+  }
 })
 
 module.exports = router
