2c3453b51fbfe5840367680d13d06097bfdb2574
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-10-01 21:21:21 -0600

    Create initial test for user creation

---
+++ tests/user.spec.js
@@ -0,0 +1,16 @@
+var shell = require('shelljs');
+var request = require("supertest");
+var app = require('./app');
+
+describe('api', () => {
+  beforeAll(() => {
+    shell.exec('npx sequelize db:create')
+  });
+  beforeEach(() => {
+      shell.exec('npx sequelize db:migrate')
+      shell.exec('npx sequelize db:seed:all')
+    });
+  afterEach(() => {
+    shell.exec('npx sequelize db:migrate:undo:all')
+  });
+});
