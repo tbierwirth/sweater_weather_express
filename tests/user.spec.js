3b4cd71e65b15bea80c2da64fd6bbd2d4d13f994
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-10-02 15:26:49 -0600

    Add tests for not creating a user if email is not unique or passwords not matching

--- tests/user.spec.js
+++ tests/user.spec.js
@@ -1,6 +1,7 @@
 var shell = require('shelljs');
 var request = require("supertest");
 var app = require('../app');
+var users = require('../models').User
 
 describe('api', () => {
   beforeAll(() => {
@@ -14,20 +15,43 @@ describe('api', () => {
   });
 
   describe('Test POST /api/v1/users path', () => {
-  test('should return a 200 status', () => {
-    return request(app).post("/api/v1/users").then(response => {
-      expect(response.status).toBe(201)
-    });
-  });
-  test('should return an api_key on creation', () => {
-    const user = {
-      email: "billgates@gmail.com",
-      password: "windows",
-      password_confirmation: "windows"
-    }
-    return request(app).post("/api/v1/users").send(user).then(response => {
-      expect(Object.keys(response.body)).toContain('api_key')
+    test('should return an api_key on creation', () => {
+      const user = {
+        email: "billgates@gmail.com",
+        password: "windows",
+        password_confirmation: "windows"
+      }
+      return request(app).post("/api/v1/users").send(user).then(response => {
+        expect(Object.keys(response.body)).toContain('api_key')
+        expect(response.status).toBe(201)
+        })
+      });
+    test('should not create an account if passwords dont match', () => {
+      const user = {
+        email: "billgates@gmail.com",
+        password: "windows",
+        password_confirmation: "window"
+      }
+      return request(app).post("/api/v1/users").send(user).then(response => {
+        expect(response.status).toBe(400)
+        expect(response.body.error).toContain('Passwords do not match')
       })
-    });
+    })
+    test('should not create an account if email is taken', () => {
+      users.create({
+        email: "billgates@gmail.com",
+        password_digest: "password",
+        api_key: "smi123o4ml124"
+      })
+      const user = {
+        email: "billgates@gmail.com",
+        password: "windows",
+        password_confirmation: "windows"
+      }
+      return request(app).post("/api/v1/users").send(user).then(response => {
+        expect(response.status).toBe(400)
+        expect(response.body.error).toContain('Email has been taken')
+      })
+    })
   });
 });
