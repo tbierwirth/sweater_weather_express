fdab145352a68389b0cb0f111883438cb2360201
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-10-01 21:31:11 -0600

    Add tests for user creation and expecting an api_key in response

--- tests/user.spec.js
+++ tests/user.spec.js
@@ -1,6 +1,6 @@
 var shell = require('shelljs');
 var request = require("supertest");
-var app = require('./app');
+var app = require('../app');
 
 describe('api', () => {
   beforeAll(() => {
@@ -12,5 +12,23 @@ describe('api', () => {
     });
   afterEach(() => {
     shell.exec('npx sequelize db:migrate:undo:all')
+  });
+
+  describe('Test POST /api/v1/users path', () => {
+  test('should return a 200 status', () => {
+    return request(app).post("/api/v1/users").then(response => {
+      expect(response.status).toBe(200)
+    });
+  });
+  test('should return an api_key on creation', () => {
+    const user = {
+      email: "billgates@gmail.com",
+      password: "windows",
+      password_confirmation: "windows"
+    }
+    return request(app).post("/api/v1/users").send(user).then(response => {
+      expect(Object.keys(response.body)).toContain('api_key')
+      })
+    });
   });
 });
