d2c86c982ffb8d58075a00c83336fcc28fd2e5b5
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-10-04 11:42:42 -0600

    Adjust how payload is being sent in response

--- routes/api/v1/sessions.js
+++ routes/api/v1/sessions.js
@@ -5,57 +5,27 @@ var users = require('../../../models').User
 const bcrypt = require('bcrypt');
 
 router.post('/', function(req, res, next) {
+  var payload;
   users.findOne({where: {email: req.body.email}})
   .then(user => {
     if (user) {
-      if (bcrypt.compareSync(req.body.password, user.password_digest) == true) {
-        api_key = {
-          api_key: user.api_key
-        }
-        res.status(200).send(api_key)
+      if (bcrypt.compareSync(req.body.password, user.password_digest)) {
+        res.status(200).send({api_key: user.api_key})
       } else {
         payload = {
           error: "Password is incorrect",
           status: 401
         }
-        res.status(401).send(JSON.stringify(payload))
+        res.status(401).send(payload)
       }
     } else {
       payload = {
         error: "Email is incorrect or does not exist",
-        status: 400
+        status: 401
       }
-      res.status(400).send(JSON.stringify(payload))
+      res.status(401).send(payload)
     }
   })
 })
-
-// router.post('/', function(req, res, next) {
-//   users.findOne({where: {email: req.body.email}})
-//   .then(user => {
-//     if (user) {
-//       bcrypt.compare(req.body.password, user.password_digest, function(err, res) {
-//         if (res == true) {
-//           api_key = {
-//             api_key: user.api_key
-//           }
-//           res.status(200).send(api_key)
-//         } else {
-//           payload = {
-//             error: "Password is incorrect",
-//             status: 401
-//           }
-//           res.status(401).send(JSON.stringify(payload))
-//         }
-//       })
-//     } else {
-//       payload = {
-//         error: "Email is incorrect or does not exist",
-//         status: 400
-//       }
-//       res.status(400).send(JSON.stringify(payload))
-//     }
-//   })
-// })
 
 module.exports = router
