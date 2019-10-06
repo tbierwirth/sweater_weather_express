feccb1dbe2ba4cb99bfc03fb1f82285babdb1818
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-10-06 13:12:08 -0600

    Create controller to fetch the forecast of a location using google geocoding and darksky api services

---
+++ routes/api/v1/forecast.js
@@ -0,0 +1,45 @@
+var express = require('express')
+var router = express.Router()
+var users = require('../../../models').User
+var DarkSkyService = require('../../../services/darksky_service').DarkSkyService
+var GoogleGeocodingService = require('../../../services/google_geocode_service').GoogleGeocodingService
+
+router.get('/', function(req, res, next) {
+  var payload;
+  users.findOne({where: {api_key: req.body.api_key}})
+  .then(user => {
+    if (user) {
+      let google = new GoogleGeocodingService(req.query.location)
+      return google.getCoordinates()
+      .then(response => {
+        let results = response.results[0]
+        var coordinates = results.geometry.location.lat + ',' + results.geometry.location.lng
+        let darksky = new DarkSkyService(coordinates)
+        return darksky.getForecast()
+        .then(response => {
+          forecast = {
+            currently: response.currently,
+            hourly: response.hourly,
+            daily: response.daily
+          }
+          res.status(200).send(forecast)
+        })
+      })
+    } else {
+      payload = {
+        error: "API key is not valid",
+        status: 401
+      }
+      res.status(401).send(payload)
+    }
+  })
+  .catch(error => {
+    payload = {
+      error: "API key is not valid",
+      status: 401
+    }
+    res.status(401).send(payload)
+  })
+})
+
+module.exports = router
