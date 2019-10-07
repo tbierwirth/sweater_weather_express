a3b044539e54e2645bddea92f4808b7bc5ade35e
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-10-07 07:50:33 -0600

    Add test for getting a users favorite locations

--- tests/favorite_locations.spec.js
+++ tests/favorite_locations.spec.js
@@ -1,6 +1,9 @@
 var shell = require("shelljs");
 var request = require("supertest");
 var app = require('../app');
+const locations = require("../models").FavoriteLocation
+const users = require("../models").User
+
 
 describe('api', () => {
   beforeAll(() => {
@@ -24,7 +27,7 @@ describe('api', () => {
         expect(response.status).toBe(200)
         expect(response.body.message).toContain('Denver, CO has been added to your favorites')
         })
-      });
+      })
 
     test('should not create a a favorite if api_key doesnt exist', () => {
       const location = {
@@ -34,7 +37,31 @@ describe('api', () => {
       return request(app).post("/api/v1/favorites").send(location).then(response => {
         expect(response.status).toBe(401)
         expect(response.body.message).toContain('API key is invalid')
-        })
+      })
     })
-  });
-});
+  })
+
+  describe('Test GET /api/v1/favorites', () =>  {
+    test('should return a users favorite locations', () => {
+      return users.create({
+        email: 'tyler@gmail.com',
+        password_digest: "t23t23t32t23t23",
+        api_key: 'this-is-my-key'
+      })
+      .then(user => {
+        return locations.bulkCreate([
+          { location: "Denver, CO", userId: user.id},
+          { location: "Topeka, KS", userId: user.id}
+        ])
+        .then(locations => {
+          const key = {
+            api_key: 'this-is-my-key'
+          }
+          return request(app).get("/api/v1/favorites").send(key).then(response => {
+            expect(response.status).toBe(200)
+          })
+        })
+      })
+    })
+  })
+})
